cd2f7281b6784196496faade037e6de4067d954b
Shane Husson <shane.husson@bigcommerce.com>
2015-12-03 12:09:23 +1100

    pteron: query psudo datbase concurrently

--- pteron.go
+++ pteron.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+  // "fmt"
+  "io/ioutil"
+  "os"
+  "strconv"
+  "strings"
+)
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func dataWorker(storeId string, results chan string) {
+  results <- queryDatabase(storeId)
+}
+
+func queryDatabase(storeId string) string {
+  return "A RESULT"
+}
+
+func queryDatabases(storeIds []string) {
+  path := "query.results"
+  err := os.RemoveAll(path)
+  check(err)
+  f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+  check(err)
+  defer f.Close()
+  results := make(chan string, len(storeIds))
+
+  for i := 0; i < len(storeIds); i++ {
+    go dataWorker(storeIds[i], results)
+  }
+
+  for i := 0; i < len(storeIds); i++ {
+    result := <-results
+    thing := strconv.Itoa(i)
+    check(err)
+    _, err = f.WriteString(result + thing)
+    check(err)
+  }
+
+}
 
 func main() {
-  fmt.Printf("Hello, bee.\n")
+  dat, err := ioutil.ReadFile("test.data")
+  check(err)
+  storeIds := strings.Split(string(dat), ",")
+  queryDatabases(storeIds)
 }
