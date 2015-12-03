eee578055d920816d651ffdc2074b7649279fd13
Shane Husson <shane.husson@bigcommerce.com>
2015-12-03 14:40:01 +1100

    add worker pool and decouple jobs from input size and support timeout

--- pteron.go
+++ pteron.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-  // "fmt"
+  "fmt"
   "io/ioutil"
   "os"
   "strconv"
   "strings"
+  "time"
 )
+
+const QUERY_TIMEOUT = 10
+const WORKER_COUNT = 100000
 
 func check(e error) {
   if e != nil {
@@ -14,11 +18,30 @@ func check(e error) {
   }
 }
 
-func dataWorker(storeId string, results chan string) {
-  results <- queryDatabase(storeId)
+func dataWorker(id int, jobs <-chan string, results chan string) {
+  for j := range jobs {
+    // create an anon go routine so we can timeout if the query takes to long
+    queryJob := make(chan string, 1)
+    go func() {
+      fmt.Println("worker", id, "processing job", j)
+      queryJob <- queryDatabase(j)
+    }()
+
+    select {
+    case result := <-queryJob:
+      results <- result
+    case <-time.After(time.Second * QUERY_TIMEOUT):
+      fmt.Println("worker", id, "timed out trying to process ", j)
+      results <- "Query Timed out"
+    }
+  }
 }
 
 func queryDatabase(storeId string) string {
+  if storeId == "2601" {
+    time.Sleep(time.Second * 20)
+  }
+  time.Sleep(time.Second * 1)
   return "A RESULT"
 }
 
@@ -31,15 +54,22 @@ func queryDatabases(storeIds []string) {
   defer f.Close()
   results := make(chan string, len(storeIds))
 
-  for i := 0; i < len(storeIds); i++ {
-    go dataWorker(storeIds[i], results)
+  jobs := make(chan string, len(storeIds))
+
+  for w := 0; w < WORKER_COUNT; w++ {
+    go dataWorker(w, jobs, results)
   }
 
   for i := 0; i < len(storeIds); i++ {
+    jobs <- storeIds[i]
+  }
+  close(jobs)
+
+  for i := 0; i < len(storeIds); i++ {
     result := <-results
-    thing := strconv.Itoa(i)
+    index := strconv.Itoa(i)
     check(err)
-    _, err = f.WriteString(result + thing)
+    _, err = f.WriteString(result + index)
     check(err)
   }
 
